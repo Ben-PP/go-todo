38d1a8a5a7fd2578aefeeff0f6aa47263c05c90c
Karel Parkkola <karel.parkkola@gmail.com>
2025-06-24 23:24:30 +0300

    Added middleware for jwt validation

---
+++ api/middleware/jwt_auth_middleware.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"fmt"
+	"go-todo/util"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JwtAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, err := util.DecodeTokenFromHeader(c)
+		if err != nil {
+			fmt.Println(err.Error())
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"status": "unauthorized",
+				"detail": "Access token validation failed.",
+			})
+			c.Abort()
+			return
+		}
+
+		c.Set("x-token-username", token.UserName)
+		c.Set("x-token-user-id", token.Subject)
+		c.Set("x-token-is-admin", token.IsAdmin)
+
+		c.Next()
+	}
+}
